f308ffe46125712af42f801761df3e0d31352b39
Miquel Ferran <miquel.ferran.gonzalez@gmail.com>
2016-06-17 01:34:17 +0200

    More Docker things.

--- packages/resort/component/docker.py
+++ packages/resort/component/docker.py
@@ -25,19 +25,44 @@ class Endpoint:
 
 		pass
 
-	def build(self, name):
+	def build(self, name, version, base_dir):
 
 		"""
 		Build an image.
 
 		:param Contextual name:
 		   Image name.
+		:param Contextual version:
+		   Image version.
+		:param Contextual base_dir:
+		   Base directory containing a ``Dockerfile``.
+		:return:
+		   Built image.
 		:rtype:
-		   Image
+		   docker.Image
 		"""
 
-		return Image(self, name)
-
+		return Image(self, name, version, base_dir)
+		
+	def create(self, image_name, image_version, name):
+	
+		"""
+		Create a container.
+		
+		:param Contextual image_name:
+		   Image name.
+		:param Contextual image_version:
+		   Image version.
+		:param Contextual name:
+		   Container name.
+		:return:
+		   Created container.
+		:rtype:
+		   docker.Container
+		"""
+		
+		return Container(self, image_name, image_version, name)
+		
 class Image:
 
 	"""
@@ -47,9 +72,64 @@ class Image:
 	   Docker endpoint.
 	:param Contextual name:
 	   Image name.
+	:param Contextual version:
+	   Image version.
+	:param Contextual base_dir:
+	   Base directory containing a ``Dockerfile``.
 	"""
-
-	def __init__(self, endpoint, name):
-
+	
+	def __init__(self, endpoint, name, version, base_dir):
+	
 		self.__endpoint = endpoint
 		self.__name = name
+		self.__version = version
+		self.__base_dir = base_dir
+		
+class Container:
+
+	"""
+	Docker container. Implements :class:`Component`.
+
+	:param docker.Endpoint endpoint:
+	   Docker endpoint.
+	:param Contextual image_name:
+	   Image name.
+	:param Contextual image_version:
+	   Image version.
+	:param Contextual name:
+	   Container name.
+	"""
+	
+	def __init__(self, endpoint, image_name, image_version, name):
+	
+		self.__endpoint = endpoint
+		self.__image_name = image_name
+		self.__image_version = image_version
+		self.__name = name
+		
+	def run(self):
+	
+		"""
+		Run a Docker container.
+		
+		:return:
+		   The running container.
+		:rtype:
+		   docker.ContainerRunning
+		"""
+		
+		return ContainerRunning(self)
+		
+class ContainerRunning:
+
+	"""
+	Docker container is running. Implements :class:`Component`.
+
+	:param docker.Endpoint endpoint:
+	   Docker endpoint.
+	"""
+	
+	def __init__(self, endpoint):
+	
+		self.__endpoint = endpoint
+
